c3e00a19de037da30082b0c199f42898ecae0d5f
Nico Schlömer <nico.schloemer@gmail.com>
2017-02-21 15:09:09 +0100

    ...

--- perfplot/__about__.py
+++ perfplot/__about__.py
@@ -2,7 +2,7 @@
 #
 __author__ = u'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
-__copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __email__)
+__copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __author_email__)
 __license__ = 'License :: OSI Approved :: MIT License'
 __version__ = '0.1.0'
 __status__ = 'Development Status :: 3 - Alpha'
