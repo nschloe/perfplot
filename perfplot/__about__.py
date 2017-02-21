e08e106c0a11e17b9145c96d7b5bf5d9ddf8e146
Nico Schlömer <nico.schloemer@gmail.com>
2017-02-21 14:25:35 +0100

    initial commit

---
+++ perfplot/__about__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+__author__ = u'Nico Schlömer'
+__email__ = 'nico.schloemer@gmail.com'
+__copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __email__)
+__license__ = 'MIT License'
+__version__ = '0.1.0'
+__status__ = 'Alpha'
---
+++ perfplot/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+from perfplot.__about__ import (
+        __author__,
+        __email__,
+        __copyright__,
+        __license__,
+        __version__,
+        __status__
+        )
+
+
+def show(setup, kernels, labels, n_range, xlabel=None, repeat=5, number=100):
+    # import inspect
+    from matplotlib import pyplot as plt
+    import numpy
+    import timeit
+
+    timings = numpy.empty((len(kernels), len(n_range), repeat))
+    for k, kernel in enumerate(kernels):
+        for i, n in enumerate(n_range):
+            out = setup(n)
+            timings[k, i] = timeit.repeat(
+                stmt=lambda: kernel(out),
+                repeat=repeat,
+                number=number
+                )
+    timings /= number
+
+    # plot minimum
+    x = n_range
+    T = numpy.min(timings, axis=2)
+    for t, label in zip(T, labels):
+        plt.plot(x, t, label=label)
+    if xlabel:
+        plt.xlabel(xlabel)
+    plt.ylabel('Time in seconds')
+    plt.legend()
+    plt.show()
+    return
