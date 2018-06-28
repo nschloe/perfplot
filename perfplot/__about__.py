01bc10b911a757aa762919eebff721b3f441687a
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-28 18:43:17 +0200

    version bump

--- perfplot/__about__.py
+++ perfplot/__about__.py
@@ -4,5 +4,5 @@ __author__ = u"Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __copyright__ = u"Copyright (c) 2017-2018, {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __status__ = "Development Status :: 5 - Production/Stable"
