23bb3b16ec6ff9fa55a98ee8774594fb519a675b
skilkis <sankilkis@msn.com>
2019-08-27 02:05:30 +0200

    Add new test for the `automatic_scale` option
    Additional Changes:
    - Reduce code similarity by making `kernels` and `r` module attributes

--- test/perfplot_test.py
+++ test/perfplot_test.py
@@ -1,11 +1,11 @@
 import numpy
-
 import perfplot
+
+kernels = [lambda a: numpy.c_[a, a]]
+r = [2 ** k for k in range(4)]
 
 
 def test():
-    kernels = [lambda a: numpy.c_[a, a]]
-    r = [2 ** k for k in range(4)]
     perfplot.show(
         setup=numpy.random.rand,
         kernels=kernels,
@@ -48,23 +48,49 @@ def test():
 
 
 def test_no_labels():
-    def mytest(a):
-        return numpy.c_[a, a]
-
-    kernels = [mytest]
-    r = [2 ** k for k in range(4)]
-
-    perfplot.plot(setup=numpy.random.rand, kernels=kernels, n_range=r, xlabel="len(a)")
+    perfplot.plot(
+        setup=numpy.random.rand,
+        kernels=kernels,
+        n_range=r,
+        xlabel="len(a)"
+    )
     return
 
 
+def test_automatic_scale():
+    from perfplot.main import PerfplotData, si_time
+
+    # (expected_prefix, time in nanoseconds, expected_timing) format
+    test_cases = [
+        ('ns', 0.125, 0.125),  # Dealing w/ edge-case when timing < nanosecond
+        ('us', 9.999e5, 999.9),  # Almost a milisecond
+        ('ms', 1e6, 1.0),  # Equal exactly to a milisecond
+        ('s', 1.5e9, 1.5),  # Over 1 second
+        ('s', 1e9, 1.0)  # Tests if disabling ``automatic_scale`` yields seconds
+    ]
+    for i, (exp_prefix, time_ns, exp_timing) in enumerate(test_cases):
+        data = PerfplotData(
+            n_range=[1],
+            # Converting timings to ns
+            timings=numpy.full((1, 1), time_ns, dtype=numpy.float64),
+            labels=[""],
+            colors=[""],
+            xlabel="",
+            title="",
+            logx=False,
+            logy=False,
+            automatic_order=True,
+            # True except for last test-case
+            automatic_scale=(True if i != len(test_cases)-1 else False)
+        )
+        # Has the correct prefix been applied?
+        assert data.timings_unit == exp_prefix
+
+        # Have the timings been updated correctly?
+        assert numpy.min(data.timings) == exp_timing
+
+
 def test_save():
-    def mytest(a):
-        return numpy.c_[a, a]
-
-    kernels = [mytest]
-    r = [2 ** k for k in range(4)]
-
     perfplot.save(
         "out.png",
         setup=numpy.random.rand,
