569a539fd772fabeeff994a87e0afa65aa8d95de
Nico Schlömer <nico.schloemer@gmail.com>
2020-02-28 12:51:23 +0100

    year update

--- perfplot/__about__.py
+++ perfplot/__about__.py
@@ -1,6 +1,6 @@
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
-__copyright__ = "Copyright (c) 2017-2019, {} <{}>".format(__author__, __author_email__)
+__copyright__ = "Copyright (c) 2017-2020, {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
 __version__ = "0.7.0"
 __status__ = "Development Status :: 5 - Production/Stable"
