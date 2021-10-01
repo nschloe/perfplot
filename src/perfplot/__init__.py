e369569549f2b706d7cbc95803f1d5fd7271d1df
Nico Schlömer <nico.schloemer@gmail.com>
2021-10-01 11:48:23 +0200

    rm __about__

--- src/perfplot/__init__.py
+++ src/perfplot/__init__.py
@@ -1,11 +1,3 @@
-from .__about__ import __version__
 from ._main import bench, live, plot, save, show
 
-__all__ = [
-    "__version__",
-    "bench",
-    "plot",
-    "show",
-    "save",
-    "live",
-]
+__all__ = ["bench", "plot", "show", "save", "live"]
