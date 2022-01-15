0d3254cc582107638549d0e8024f5495d03ff43e
Nico Schlömer <nico.schloemer@gmail.com>
2022-01-15 11:10:20 +0100

    setuptools -> flit

--- src/perfplot/__init__.py
+++ src/perfplot/__init__.py
@@ -1,3 +1,5 @@
 from ._main import bench, live, plot, save, show
 
-__all__ = ["bench", "plot", "show", "save", "live"]
+__version__ = "0.9.14"
+
+__all__ = ["bench", "plot", "show", "save", "live", "__version__"]
